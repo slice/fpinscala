885c3aadde114df7fa34a871a56705fcc1924ca9
slice <ryaneft@gmail.com>
2020-07-20 18:47:04 -0700

    2.3 to 2.5

---
+++ Currying.scala
@@ -0,0 +1,13 @@
+object Currying extends App {
+  // Exercise 2.3
+  def curry[A, B, C](f: (A, B) => C): A => B => C =
+    a => b => f(a, b)
+
+  // Exercise 2.4
+  def uncurry[A, B, C](f: A => B => C): (A, B) => C =
+    (a, b) => f(a)(b)
+
+  // Exercise 2.5
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
+}
