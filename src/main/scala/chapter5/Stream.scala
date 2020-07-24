2f14502498bfaba47b269e621b77b2b067c27657
slice <ryaneft@gmail.com>
2020-07-23 19:09:20 -0700

    5.7 to 5.16

--- src/main/scala/chapter5/Stream.scala
+++ src/main/scala/chapter5/Stream.scala
@@ -72,9 +72,79 @@ sealed trait Stream[+A] {
   // Exercise 5.6
   def headOption2: Option[A] =
     foldRight(None: Option[A])((a, _) => Some(a))
+
+  // Exercise 5.7
+  def map[B](f: A => B): Stream[B] =
+    foldRight(Empty: Stream[B])((a, b) => Stream.cons(f(a), b))
+  def filter(p: A => Boolean): Stream[A] =
+    foldRight(Empty: Stream[A])((a, b) => if (p(a)) Stream.cons(a, b) else b)
+  def append[AA >: A](o: => Stream[AA]): Stream[AA] =
+    foldRight(o)((a, b) => Stream.cons(a, b))
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(Empty: Stream[B])((a, b) => f(a).append(b))
+
+  // Exercise 5.13
+  def map2[B](f: A => B): Stream[B] =
+    Stream.unfold(this) {
+      case Cons(h, t) => Some(f(h()), t())
+      case Empty      => None
+    }
+  def take2(n: Int): Stream[A] =
+    Stream.unfold((n, this)) {
+      case (0, _)          => None
+      case (n, Cons(h, t)) => Some((h(), (n - 1, t())))
+      case (_, Empty)      => None
+    }
+  def takeWhile3(p: A => Boolean): Stream[A] =
+    Stream.unfold(this) {
+      case Cons(h, t) if p(h()) => Some((h(), t()))
+      case _                    => None
+    }
+  def zipWith[B, C](b: Stream[B])(f: (A, B) => C): Stream[C] =
+    Stream.unfold((this, b)) {
+      case (Cons(h, t), Cons(h2, t2)) => Some((f(h(), h2()), (t(), t2())))
+      case _                          => None
+    }
+  def zipAll[B](b: Stream[B]): Stream[(Option[A], Option[B])] =
+    Stream.unfold((this, b)) {
+      case (Cons(h, t), Cons(h2, t2)) =>
+        Some((Some(h()), Some(h2())), (t(), t2()))
+      case (Cons(h, t), Empty) =>
+        Some((Some(h()), None), (t(), Empty))
+      case (Empty, Cons(h, t)) =>
+        Some((None, Some(h())), (Empty, t()))
+      case (Empty, Empty) =>
+        None
+    }
+
+  // Exercise 5.14
+  def startsWith[A](s: Stream[A]): Boolean =
+    Stream
+      .unfold((this, s)) {
+        case (Cons(hh, ht), Cons(nh, nt)) =>
+          Some((hh() == nh(), (ht(), nt())))
+        case _ => None
+      }
+      .takeWhile(identity)
+      .toList
+      .size >= s.toList.size
+
+  // Exercise 5.15
+  def tails: Stream[Stream[A]] =
+    Stream.unfold(this) {
+      case s @ Cons(h, t) => Some(s, t())
+      case _              => None
+    }
+
+  def hasSubsequence[A](s: Stream[A]): Boolean =
+    tails exists (_ startsWith s)
+
+  // Exercise 5.16
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    this.tails.map(_.foldRight(z)(f)).append(Stream(z))
 }
 
-case object Empty extends Stream[Nothing]
+case object Empty                                   extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
@@ -88,4 +158,38 @@ object Stream {
 
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
+  // Exercise 5.8
+  def constant[A](a: A): Stream[A] =
+    cons(a, constant(a))
+
+  // Exercise 5.9
+  def from(n: Int): Stream[Int] =
+    cons(n, from(n + 1))
+
+  // Exercise 5.10
+  def fibs: Stream[Int] = {
+    def go(a: Int, b: Int): Stream[Int] =
+      Stream.cons(a + b, go(b, a + b))
+    Stream.cons(0, Stream.cons(1, go(0, 1)))
+  }
+
+  // Exercise 5.11
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z) match {
+      case Some((value, state)) => Stream.cons(value, unfold(state)(f))
+      case None                 => Empty
+    }
+
+  // Exercise 5.12
+  def fibs2: Stream[Int] =
+    Stream(0, 1).append(unfold((0, 1)) {
+      case (l, r) => Some((l + r, (r, l + r)))
+    })
+  def from2(n: Int): Stream[Int] =
+    unfold(n)(s => Some((s, s + 1)))
+  def constant2[A](a: A): Stream[A] =
+    unfold(a)(_ => Some((a, a)))
+  def ones2: Stream[Int] =
+    constant2(1)
 }
--- src/test/scala/Chapter5Suite.scala
+++ src/test/scala/Chapter5Suite.scala
@@ -2,6 +2,7 @@ package zone.slice.fpinscala
 
 class Chapter5Suite extends munit.FunSuite {
   import chapter5._
+
   val stream = Stream(1, 2, 3)
 
   test("5.1") {
@@ -29,5 +30,80 @@ class Chapter5Suite extends munit.FunSuite {
   test("5.6") {
     assertEquals(stream.headOption2, Some(1))
     assertEquals(Stream.empty.headOption2, None)
+  }
+
+  test("5.7") {
+    assertEquals(stream.map(_ + 1).toList, List(2, 3, 4))
+    assertEquals(stream.filter(_ < 3).toList, List(1, 2))
+    assertEquals(stream.append(Stream(4, 5, 6)).toList, List(1, 2, 3, 4, 5, 6))
+    assertEquals(
+      stream.flatMap(_ => stream).toList,
+      List(1, 2, 3, 1, 2, 3, 1, 2, 3)
+    )
+  }
+
+  test("5.8") {
+    assertEquals(Stream.constant(1).take(5).toList, List.fill(5)(1))
+    assertEquals(Stream.constant(1).exists(_ == 1), true)
+  }
+
+  test("5.9") {
+    assertEquals(Stream.from(1).take(5).toList, List(1, 2, 3, 4, 5))
+  }
+
+  test("5.10") {
+    assertEquals(Stream.fibs.take(8).toList, List(0, 1, 1, 2, 3, 5, 8, 13))
+  }
+
+  test("5.11") {
+    val ints = Stream.unfold(0)(n => Some((n, n + 1)))
+    assertEquals(ints.take(5).toList, List(0, 1, 2, 3, 4))
+    val countDown =
+      Stream.unfold(5)(n => if (n == 0) None else Some((n, n - 1)))
+    assertEquals(countDown.toList, List(5, 4, 3, 2, 1))
+  }
+
+  test("5.12") {
+    assertEquals(Stream.fibs2.take(20).toList, Stream.fibs.take(20).toList)
+    assertEquals(
+      Stream.from2(2).take(10).toList,
+      Stream.from(2).take(10).toList
+    )
+    assertEquals(Stream.constant2(5).take(5).toList, List.fill(5)(5))
+    assertEquals(Stream.ones2.take(5).toList, List.fill(5)(1))
+  }
+
+  test("5.13") {
+    assertEquals(stream.map2(_ + 1).toList, List(2, 3, 4))
+    assertEquals(stream.take2(2).toList, List(1, 2))
+    assertEquals(stream.takeWhile3(_ < 3).toList, List(1, 2))
+    assertEquals(stream.zipWith(stream)(_ + _).toList, List(2, 4, 6))
+    assertEquals(
+      stream.zipAll(Stream(1, 2, 3, 4, 5)).toList,
+      List(
+        (Some(1), Some(1)),
+        (Some(2), Some(2)),
+        (Some(3), Some(3)),
+        (None, Some(4)),
+        (None, Some(5))
+      )
+    )
+  }
+
+  test("5.14") {
+    assertEquals(stream.startsWith(Stream(1, 2)), true)
+    assertEquals(stream.startsWith(Stream(1, 3)), false)
+  }
+
+  test("5.15") {
+    assertEquals(
+      stream.tails.toList.map(_.toList),
+      List(List(1, 2, 3), List(2, 3), List(3))
+    )
+    assertEquals(stream.hasSubsequence(Stream(2, 3)), true)
+  }
+
+  test("5.16") {
+    assertEquals(stream.scanRight(0)(_ + _).toList, List(6, 5, 3, 0))
   }
 }
